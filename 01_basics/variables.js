9afbd6dbb3c4cd9c7ec5890fde42bb077dc064b2
Deep Das <161225559+RipDeep@users.noreply.github.com>
2025-07-06 16:01:00 +0000

    Variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,18 @@
+const accountId=54857
+let accountEmail="TFdtyvv"
+var accountPass="159745"
+accountCity="Burdwan"
+
+//accountId=2 //not allowed
+accountEmail="ahbhs"
+accountPass="888888"
+accountCity="bar"
+console.log(accountId);
+console.log(accountEmail);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPass, accountCity])
+
