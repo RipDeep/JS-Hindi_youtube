d3be5ec8cebba0244cc078101475448ec7e38995
Deep Das <161225559+RipDeep@users.noreply.github.com>
2025-07-07 13:47:17 +0000

    data types and converstion

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";  //treat all the JS code as newer version
+
+//alert(3+3) //er are using node js not browser
+
+console.log(3
+    +3) //code readability should be high
+console.log("Deep");
+
+
+let name="Deep Das"
+let age=21
+let isLoggedIn=false
+let state //undefined
+let city = null //null
+
+//number => 2 to the power 53
+//bigint 
+//string=>""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);  //object
+
+
