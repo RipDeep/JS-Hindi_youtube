0f6dd9b6254dd3ebc5300385f25475982cbab2f1
Deep Das <161225559+RipDeep@users.noreply.github.com>
2025-07-07 14:26:43 +0000

    String convertion

---
+++ 01_basics/convertionOperations.js
@@ -0,0 +1,45 @@
+let value =3 
+let negValue=-value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+
+let str1="hello"
+let str2 = " world"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+
+console.log("1" + 2+ 2) //122
+console.log(1 + 2+ "2") //32
+
+
+console.log((3+4)*5%3); //not good code
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3 
+
+num1=num2=num3=2+2
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+let gameCount =100
+console.log(gameCount);
+
+gameCount++;
+console.log(gameCount);
+
+++gameCount;
+console.log(gameCount);
+
