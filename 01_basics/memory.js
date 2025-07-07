e088fc2c1ba40b4f6e140e336e148b4133b7b01c
Deep Das <161225559+RipDeep@users.noreply.github.com>
2025-07-07 15:07:34 +0000

    memory

---
+++ 01_basics/memory.js
@@ -0,0 +1,23 @@
+//stack(premitive), Heap(non-premitive)
+
+let myYoutubeName="hitesh"
+
+let anotherName=myYoutubeName
+anotherName="chaiAurCode"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne={
+    email:"user@gmail.com",
+    upi: "deep8998"
+}
+
+let userTwo =userOne
+
+userTwo.email="Das@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
