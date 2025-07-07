2144cc659f0482dd2996c7b0e234b325fc3bb597
Deep Das <161225559+RipDeep@users.noreply.github.com>
2025-07-07 14:35:55 +0000

    comparison operator

---
+++ 01_basics/comparison.js
@@ -0,0 +1,27 @@
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2!=1)
+console.log(2==1)
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined ==0);
+console.log(undefined >0);
+console.log(undefined <0);
+
+//comparison and equality opeartor work differently
+//in comparison it change the NULL in 0
+//in equality it same as it null
+
+console.log("2" === 2);
+
+// === checks the data types also
+
+
+
