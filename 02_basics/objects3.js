40f0ff52ea2bdb0cfe1d897bd9aa4ca3aabbe4d6
Deep Das <161225559+RipDeep@users.noreply.github.com>
2025-07-09 15:38:38 +0000

    objects

---
+++ 02_basics/objects3.js
@@ -0,0 +1,34 @@
+//Destructuring
+
+const course ={
+    courseName:"js in hindi",
+    price:"199",
+    courseInstructor:"Hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor}= course
+console.log(instructor);
+
+
+// const navbar =({company})=>{
+
+// }
+// navbar(company ="Deep")
+
+// {
+//     "name":"Deep",         //json structure
+//     "courseName1":"JS",
+//     "price":"5959"
+// }
+
+
+// [
+//     {},
+//     {},
+//     {},
+//     {},
+//     {},
+// ]
+
